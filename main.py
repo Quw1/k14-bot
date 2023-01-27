78f9b6379464c06a22ff17696626b75aa87cbe08
Quw1 <itsrerades@gmail.com>
2023-01-27 19:24:01 +0300

    rel-polling

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 import os
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
+# from aiogram.utils.executor import start_webhook
+from aiogram.utils import executor
 from aiogram import Bot, types
 
 
@@ -11,23 +12,23 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+# HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 CHANNEL_ID1 = os.getenv('CHANNEL_ID1')
 CHANNEL_ID2 = os.getenv('CHANNEL_ID2')
 
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+# WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+# WEBHOOK_PATH = f'/webhook/{TOKEN}'
+# WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+# WEBAPP_HOST = '0.0.0.0'
+# WEBAPP_PORT = os.getenv('PORT', default=8000)
 
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+# async def on_startup(dispatcher):
+#     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
+# async def on_shutdown(dispatcher):
+#     await bot.delete_webhook()
 
 
 #----------------------------------------------------------------
@@ -53,7 +54,7 @@ async def echo_message(msg: types.Message):
 
     if msg.text:
         await bot.send_message(CHANNEL_ID2, msg.text)
-    if msg.photo:
+    elif msg.photo:
         ph = msg.photo
         await bot.send_photo(CHANNEL_ID2, ph[len(ph) - 1].file_id, caption=msg.caption)
     elif msg.document:
@@ -64,14 +65,17 @@ async def echo_message(msg: types.Message):
 
 #----------------------------------------------------------------
 
+# if __name__ == '__main__':
+#     logging.basicConfig(level=logging.INFO)
+#     start_webhook(
+#         dispatcher=dp,
+#         webhook_path=WEBHOOK_PATH,
+#         skip_updates=True,
+#         on_startup=on_startup,
+#         on_shutdown=on_shutdown,
+#         host=WEBAPP_HOST,
+#         port=WEBAPP_PORT,
+#     )
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dp)
